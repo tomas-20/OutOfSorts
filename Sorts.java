0ff4c84faace5ed0d40dca9c42c7c8b248c22a11
tomas-20 <tacuna20@stuy.edu>
2020-12-15 13:57:38 -0500

    added the bubbleSort

--- Sorts.java
+++ Sorts.java
@@ -1,4 +1,20 @@
+import java.util.Arrays;
 public class Sorts {
   public static void bubbleSort(int[] data) {
+    boolean changed = true;
+    for (int i = data.length - 1; i >= 0 && changed; i --) {
+      System.out.println(Arrays.toString(data));
+      changed = false;
+      for (int j = 0; j < i; j ++) {
+        int j2 = j + 1;
+        int current = data[j];
+        int next = data[j2];
+        if (current > next) {
+          changed = true;
+          data[j] = next;
+          data[j2] = current;
+        }
+      }
+    }
   }
 }
---
+++ Test.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Test {
+  private static void test(int[] arr) {
+    Sorts.bubbleSort(arr);
+    System.out.println(Arrays.toString(arr));
+    System.out.println();
+  }
+  public static void main(String[] args) {
+    int[] bob = {5, 1, 12, -5, 16};
+    int[] joe = {2, 3, 4, 5, 1};
+    int[] juan = {6, 1, 2, 3, 4, 5};
+    test(bob);
+    test(joe);
+    test(juan);
+  }
+}
