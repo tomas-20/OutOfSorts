0edf7deddeea3c8b8a11b3fa912d21c686663730
tomas-20 <tacuna20@stuy.edu>
2020-12-15 13:59:34 -0500

    removed the printing thingy in bubbleSort

--- Sorts.java
+++ Sorts.java
@@ -3,7 +3,6 @@ public class Sorts {
   public static void bubbleSort(int[] data) {
     boolean changed = true;
     for (int i = data.length - 1; i >= 0 && changed; i --) {
-      System.out.println(Arrays.toString(data));
       changed = false;
       for (int j = 0; j < i; j ++) {
         int j2 = j + 1;
